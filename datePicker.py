50f4c80cefb8cbd9190e47097b57156f1c7db53b
unknown <dmow@WS-KIT-5828.brockhome.net>
2014-08-29 14:00:55 -0700

    Two buttons in a row

--- datePicker.py
+++ datePicker.py
@@ -1,28 +1,29 @@
 #!/usr/bin/env python 
 
-import Tkinter as tk
+from Tkinter import *
 
-class Application(tk.Frame):
+class Application:
     def __init__(self, master=None):
-        tk.Frame.__init__(self, master)
-        self.grid(sticky=tk.N+tk.S+tk.E+tk.W)
-        self.createWidgets()
+        frame = Frame(master)
+        frame.pack()
 
-    def createWidgets(self):
-        top=self.winfo_toplevel()
-        top.rowconfigure(0, weight=1)
-        top.columnconfigure(0, weight=1)
-        self.rowconfigure(0, weight=1)
-        self.columnconfigure(0, weight=1)
-        self.quitButton = tk.Button(self, text='Quit', command=self.quit)
-        self.quitButton.grid(row=0, column=0, sticky=tk.N+tk.S+tk.E+tk.W)
+        self.button = Button(frame, text="QUIT", fg="red", command=frame.quit)
+        self.button.pack(side=LEFT)
+
+        self.hi_there = Button(frame, text="Hello", command=frame.quit)
+        self.hi_there.pack(side=LEFT)
+		
+#	def say_hi(self):
+#		print "hi there, everyone!"
 
 def main():
 
-	print "Hello World"
-	app = Application()
-	app.master.title('Sample application')
-	app.mainloop()
+	root = Tk()
+
+	app = Application(root)
+
+	root.mainloop()
+	root.destroy() # optional; see description below
 	
 
 if __name__ == "__main__":
